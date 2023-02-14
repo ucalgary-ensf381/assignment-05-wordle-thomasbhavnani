2f1e5d2361c642da75e6694d9e164e6dc6d49b22
thomasbhavnani <thomas.bhavnani@ucalgary.ca>
2023-02-13 20:46:53 -0700

    finished index.html
    started wordle.js,  style.css

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,3 +1,30 @@
-// remove this line
-// it's just here to shows it works
-window.alert("works!");
+var height = 4; //guesses
+var width = 4; //word len
+
+var row = 0;
+var column = 0;
+
+var endGame = false;
+var word = "SQUID";
+
+//put array of words from dictionary
+
+window.onload = function(){
+    initialize();
+}
+
+
+function initialize();{
+    //make board
+    for(r = 0; r < height; r++){
+        for(let c = 0; c < width; c++){
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "P";
+            document.getElementById("interface").appendChild(tile);
+            
+        }
+    }
+}
+
