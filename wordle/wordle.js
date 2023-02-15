6ecf414b11814d35291876a34ad8863819513413
thomasbhavnani <thomas.bhavnani@ucalgary.ca>
2023-02-14 18:46:43 -0700

    fixed the yellow tile logic

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -6,6 +6,15 @@ var column = 0;
 
 
 var correctWord = "BOOK";
+
+letterDictCorrect = {}
+letterDictUser = {}
+//makes dict 
+for(let i = 0; i < width; i++){
+    letterDictCorrect[correctWord.charAt(i)] = (correctWord.match(new RegExp(correctWord.charAt(i), "g")) || []).length;
+}
+
+
 var endGame = false;
 
 //put array of words from dictionary
@@ -59,10 +68,15 @@ function initialize(){
                 let nowTile = document.getElementById(row.toString() + '-' + column.toString());
                 nowTile.innerText = "";
             }
-            else if(e.code == "Enter" ){
-                update();
-                row += 1;
-                column = 0;
+            else if(e.code == "Enter"){
+                if(column != width){
+
+                }
+                else{
+                    check();
+                    row += 1;
+                    column = 0;
+                }
             }
             if(!endGame && row == height){
                 endGame = true;
@@ -72,21 +86,37 @@ function initialize(){
         })
 }
 
-function update(){
-    let rightLetter = 0;
+function check(){
+    let rightLetters = 0;
+    for(let i = 0; i < width; i++){
+        letterDictUser[correctWord.charAt(i)] = 0;
+    }
+    
     for(let col = 0; col < width; col++){
-        let nowTile = document.getElementById(row.toString() + '-' + column.toString());
+
+        let nowTile = document.getElementById(row.toString() + '-' + col.toString());
         let character = nowTile.innerText;
 
-        if(word[col] == character){
-            tile.classList.add("green-tile");
-            rightLetter += 1;
+        if(correctWord[col] == character){
+            nowTile.classList.add("green-tile");
+            rightLetters += 1;
+            letterDictUser[character] += 1
         }
-        else if(word.includes(character)){
-            tile.classList.add("yellow-tile")
+        if(rightLetters == width){
+            endGame = true;
         }
-        else{
-            tile.classList.add("gray-tile")
+    }
+    for(let col = 0; col < width; col++){
+        let nowTile = document.getElementById(row.toString() + '-' + col.toString());
+        let character = nowTile.innerText;
+        console.log(letterDictUser[character])
+        
+        if(correctWord.includes(character) && letterDictUser[character] < letterDictCorrect[character]){
+            nowTile.classList.add("yellow-tile");
+            letterDictUser[character] += 1
         }
+        else if(!nowTile.classList.contains('green-tile') && !nowTile.classList.contains('yellow-tile')){
+            nowTile.classList.add("gray-tile")
+        }   
     }
 }
