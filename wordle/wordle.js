6ca0884ddbafe12a7a8de29916aab50cccf1fc10
thomasbhavnani <thomas.bhavnani@ucalgary.ca>
2023-02-16 19:30:18 -0700

    implemented dark mode and hints

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -5,14 +5,33 @@ var row = 0;
 var column = 0;
 
 
-var correctWord = "BOOK";
+var wordList = null;
+var word;
+var hint;
 
-letterDictCorrect = {}
-letterDictUser = {}
-//makes dict 
-for(let i = 0; i < width; i++){
-    letterDictCorrect[correctWord.charAt(i)] = (correctWord.match(new RegExp(correctWord.charAt(i), "g")) || []).length;
+async function getWordAndHint(){
+    if(wordList == null){
+        const res = await fetch("https://api.masoudkf.com/v1/wordle", {
+            headers: {
+            "x-api-key": "sw0Tr2othT1AyTQtNDUE06LqMckbTiKWaVYhuirv",
+            },
+        });
+        let temp = await res.json();
+        var {dictionary} = temp;
+        wordList = dictionary;
+    }
 }
+
+async function setWordHint(){
+    getWordAndHint().then(() => {
+        var wordAndHint = wordList[Number.parseInt(Math.random() * wordList.length)];
+        word = wordAndHint.word.toUpperCase();
+        hint = wordAndHint.hint;
+  })
+}
+
+
+
 
 
 var endGame = false;
@@ -20,7 +39,10 @@ var endGame = false;
 //put array of words from dictionary
 
 window.onload = function(){
+    setWordHint();
     initialize();
+    
+
 }
 
 
@@ -44,7 +66,10 @@ function initialize(){
         console.log(rowsDiv)
     }
     document.addEventListener("keyup", (e) => {
-        if (endGame) return;
+        if (endGame){
+
+            return;
+        } 
         
         if("KeyA" <= e.code && e.code <= "KeyZ"){
             console.log(e.code);
@@ -80,16 +105,30 @@ function initialize(){
             }
             if(!endGame && row == height){
                 endGame = true;
-                document.getElementById("correct-answer").innerText = correctWord;
+                document.getElementById("correct-answer").innerText = word;
+                losscall();
             }
 
         })
 }
 
 function check(){
+
     let rightLetters = 0;
+    letterDictCorrect = {}
+    letterDictUser = {}
+    
+
+    
     for(let i = 0; i < width; i++){
-        letterDictUser[correctWord.charAt(i)] = 0;
+        letterDictCorrect[word.charAt(i)] = (word.match(new RegExp(word.charAt(i), "g")) || []).length;
+        console.log(letterDictCorrect)
+    }
+
+
+
+    for(let i = 0; i < width; i++){
+        letterDictUser[word.charAt(i)] = 0;
     }
     
     for(let col = 0; col < width; col++){
@@ -97,7 +136,7 @@ function check(){
         let nowTile = document.getElementById(row.toString() + '-' + col.toString());
         let character = nowTile.innerText;
 
-        if(correctWord[col] == character){
+        if(word[col] == character){
             nowTile.classList.add("green-tile");
             rightLetters += 1;
             letterDictUser[character] += 1
@@ -111,7 +150,7 @@ function check(){
         let character = nowTile.innerText;
         console.log(letterDictUser[character])
         
-        if(correctWord.includes(character) && letterDictUser[character] < letterDictCorrect[character] && !nowTile.classList.contains("green-tile")){
+        if(word.includes(character) && letterDictUser[character] < letterDictCorrect[character] && !nowTile.classList.contains("green-tile")){
             nowTile.classList.add("yellow-tile");
             letterDictUser[character] += 1;
         }
@@ -122,4 +161,27 @@ function check(){
 
         }   
     }
+}
+
+
+function darkMode(){
+    document.body.classList.toggle('dark-mode')
+}
+
+
+
+
+function hintcall(){
+    
+    document.getElementById("hintid").classList.add("hint");
+    document.getElementById("hintid").innerText= hint;
+}
+
+function losscall(){
+        document.getElementById("hintid").classList.add("loss");
+        document.getElementById("hintid").innerText= "you lost";
+}
+
+function displaywin(){
+    document.getElementById("interface").classList
 }
